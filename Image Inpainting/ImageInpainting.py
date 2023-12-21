7f70585c93d90cf0657f0d8e628cb0a5b077872c
EricChen0313 <93503185+EricChen0313@users.noreply.github.com>
2023-12-21 14:59:00 +0800

    Update ImageInpainting.py
    2023.12.21.14.58

--- Image Inpainting/ImageInpainting.py
+++ Image Inpainting/ImageInpainting.py
@@ -9,29 +9,27 @@ from net import PConvUNet
 from util.io import load_ckpt
 
 pygame.init()
-
 width, height = 512, 512
 size = (256, 256)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Mask Creator")
-
 background_image = pygame.image.load('input.jpg')
 background_image = pygame.transform.scale(background_image, (512, 512))
-
 black = (0, 0, 0)
 white = (255, 255, 255)
-
 lines = []
 DRAW = False
 last_pos = (0, 0)
-
+pen_width = 5
 device = torch.device('cpu')
 model = PConvUNet().to(device)
 load_ckpt('1000000.pth', [('model', model)])
 model.eval()
 
 print("Press S To Save the Mask.")
+print("Default Pen Size is :  5")
 screen.blit(background_image, (0, 0))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:      # PRESSED X
@@ -43,13 +41,19 @@ while True:
                 white_background.fill(white)
 
                 for line in lines:
-                    pygame.draw.line(white_background, black, line[0], line[1], 5)
+                    pygame.draw.line(white_background, black, line[0], line[1], line[2])
 
                 pygame.image.save(white_background, "Mask.jpg")
                 print("Mask Saved.")
                 pygame.quit()
                 EvaluateSingle(model, size, 'input.jpg', 'Mask.jpg', device)
                 sys.exit()
+            elif event.key == pygame.K_d:
+                pen_width += 5
+                print(f"Current Pen Size is : {pen_width}")
+            elif event.key == pygame.K_a:
+                pen_width = max(1, pen_width - 5)
+                print(f"Current Pen Size is : {pen_width}")
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             DRAW = True
@@ -61,9 +65,9 @@ while True:
         elif event.type == pygame.MOUSEMOTION and DRAW:
             current_pos = pygame.mouse.get_pos()
             if last_pos != (0, 0):
-                pygame.draw.line(screen, black, last_pos, current_pos, 5)
-
-                lines.append((last_pos, current_pos))
+                pygame.draw.line(screen, black, last_pos, current_pos, pen_width)
+                pygame.display.flip()
+                lines.append((last_pos, current_pos, pen_width))
             last_pos = current_pos
 
     pygame.display.flip()
